4edc88a7c36ebf9949357a03c40e2c7e6610a071
alriefqy <ekydesmito@gmail.com>
2025-07-04 09:31:01 +0800

    update echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,9 +6,6 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: import.meta.env.VITE_PUSHER_HOST || window.location.hostname,
-    wsPort: import.meta.env.VITE_PUSHER_PORT || 6001,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: false,
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
 });
