57e8e8ecd95e30c8f2ab4d35bffce779a184b5b3
alriefqy <ekydesmito@gmail.com>
2025-07-17 11:22:09 +0800

    add additional migration

---
+++ database/migrations/2025_07_17_024606_update_projects_table-add_risk_forecast.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('risk_forecast')->nullable();
+            $table->renameColumn('risk','risk_residual')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->renameColumn('risk_residual','risk_forecast');
+            $table->dropColumn('risk_forecast');
+        });
+    }
+};
