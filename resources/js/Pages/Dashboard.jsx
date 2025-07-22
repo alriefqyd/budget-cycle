89463d945e818d69e65ea79840ad41ff46b498c9
alriefqy <ekydesmito@gmail.com>
2025-07-22 11:50:01 +0800

    update legend

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -22,7 +22,7 @@ export default function Dashboard() {
                 type: 'bar',
                 xKey: 'year',
                 yKey: 'plan',
-                yName: 'Plan 2026–2030',
+                yName: 'Approved 2025–2029',
                 fill: '#007B82',
                 grouped: true,
                 label: {
@@ -38,7 +38,7 @@ export default function Dashboard() {
                 type: 'bar',
                 xKey: 'year',
                 yKey: 'approved',
-                yName: 'Approved 2025–2029',
+                yName: 'Plan 2026–2030',
                 fill: '#F4B740',
                 grouped: true,
                 label: {
