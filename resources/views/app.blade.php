c063cd10fe686b6a60ef5a93fc363e714fcc28e2
alriefqy <ekydesmito@gmail.com>
2025-07-28 08:39:35 +0800

    update tab title logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title inertia>{{ config('app.name', 'Budget Cycle 5YP') }}</title>
-
+        <!-- SVG favicon (modern browsers) -->
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('vale-logo.svg') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
