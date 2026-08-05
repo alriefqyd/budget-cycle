ad00c8c64872abaef5c0f5abac3981837a045957
alriefqy <ekydesmito@gmail.com>
2026-08-05 09:29:46 +0800

    fix trash icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
